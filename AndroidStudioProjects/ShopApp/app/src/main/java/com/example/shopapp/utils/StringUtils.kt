f7ddfe05454260b2358ee8ad8236eaeca4f92968
Aleksandra <akkovaleva14@gmail.com>
2025-02-09 23:59:35 +0300

    fix

--- AndroidStudioProjects/ShopApp/app/src/main/java/com/example/shopapp/utils/StringUtils.kt
+++
@@ -1,9 +0,0 @@
-package com.example.shopapp.utils
-
-object StringUtils {
-    // Function to validate email format using regex
-    fun isValidEmail(email: String): Boolean {
-        val emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$"
-        return email.matches(emailRegex.toRegex())
-    }
-}
